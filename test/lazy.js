afd68ae043a8fd039e7ae382625cb4a4b42f0cb8
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-09-25 11:09:00 +0200

    filtering uce-template

--- test/lazy.js
+++ test/lazy.js
@@ -3,15 +3,17 @@ addEventListener(
   function () {
     uceLoader('.', {
       loader: function (path, name) {
-        var xhr = new XMLHttpRequest;
-        xhr.open('get', path + name + '.uce', true);
-        xhr.send(null);
-        xhr.onload = function () {
-          document.body.appendChild(
-            customElements.get('uce-template')
-                          .from(xhr.responseText)
-          );
-        };
+        if (name !== 'uce-template') {
+          var xhr = new XMLHttpRequest;
+          xhr.open('get', path + name + '.uce', true);
+          xhr.send(null);
+          xhr.onload = function () {
+            document.body.appendChild(
+              customElements.get('uce-template')
+                            .from(xhr.responseText)
+            );
+          };
+        }
       }
     });
   },
