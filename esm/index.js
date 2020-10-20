06da7eb8c6bb0e6b9976e40e234ba9c32ccd7a71
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-10-20 22:53:53 +0200

    an XPath based implementation

--- esm/index.js
+++ esm/index.js
@@ -2,6 +2,21 @@
 const ignore = /^(?:annotation-xml|color-profile|font-face(?:|-format|-name|-src|-uri)|missing-glyph)$/i;
 const loaded = new Set;
 
+const {ORDERED_NODE_SNAPSHOT_TYPE} = XPathResult;
+const evaluator = new XPathEvaluator;
+const expression = evaluator.createExpression(
+  './/*[@is or contains(name(),"-")]',
+  null
+);
+
+const add = (options, node) => {
+  const is = (node.getAttribute('is') || node.tagName).toLowerCase();
+  if (!loaded.has(is) && 0 < is.indexOf('-') && !ignore.test(is)) {
+    loaded.add(is);
+    options.on(is);
+  }
+};
+
 /**
  * Start observing a document, or a specific container, and automatically
  * download once Custom Elements from a specific path
@@ -9,30 +24,33 @@ const loaded = new Set;
  * with `container`, `document` by default, and `extension`, `".js"` by default
  * @returns {MutationObserver} the disconnect-able `container` observer
  */
-export default (options) => {
+export default options => {
   const target = options.container || document;
-  const load = mutations => {
-    for (let i = 0, {length} = mutations; i < length; i++) {
-      for (let
-        {addedNodes} = mutations[i],
-        j = 0, {length} = addedNodes; j < length; j++
-      ) {
-        const node = addedNodes[j];
-        const {children, getAttribute, tagName} = node;
-        if (getAttribute) {
-          const is = (getAttribute.call(node, 'is') || tagName).toLowerCase();
-          if (0 < is.indexOf('-') && !loaded.has(is) && !ignore.test(is)) {
-            loaded.add(is);
-            options.on(is);
-          }
-          crawl(children);
-        }
-      }
-    }
-  };
-  const crawl = addedNodes => { load([{addedNodes}]) };
-  crawl(document == target ? target.documentElement.children : [target]);
-  const observer = new MutationObserver(load);
+  const bound = all.bind(options);
+  bound([{
+    addedNodes: document == target ?
+                  target.documentElement.children : [target]
+  }]);
+  const observer = new MutationObserver(bound);
   observer.observe(target, {subtree: true, childList: true});
   return observer;
 };
+
+function all(mutations) {
+  for (let i = 0, {length} = mutations; i < length; i++) {
+    for (let
+      {addedNodes} = mutations[i],
+      j = 0, {length} = addedNodes; j < length; j++
+    ) {
+      const node = addedNodes[j];
+      if (node.getAttribute)
+        add(this, node);
+      for (let
+        result = expression.evaluate(node, ORDERED_NODE_SNAPSHOT_TYPE, null),
+        {snapshotLength} = result,
+        k = 0; k < snapshotLength; k++
+      )
+        add(this, result.snapshotItem(k));
+    }
+  }
+}
--- index.js
+++ index.js
@@ -4,6 +4,19 @@ self.uceLoader = (function (exports) {
   // https://html.spec.whatwg.org/multipage/custom-elements.html#valid-custom-element-name
   var ignore = /^(?:annotation-xml|color-profile|font-face(?:|-format|-name|-src|-uri)|missing-glyph)$/i;
   var loaded = new Set();
+  var _XPathResult = XPathResult,
+      ORDERED_NODE_SNAPSHOT_TYPE = _XPathResult.ORDERED_NODE_SNAPSHOT_TYPE;
+  var evaluator = new XPathEvaluator();
+  var expression = evaluator.createExpression('.//*[@is or contains(name(),"-")]', null);
+
+  var add = function add(options, node) {
+    var is = (node.getAttribute('is') || node.tagName).toLowerCase();
+
+    if (!loaded.has(is) && 0 < is.indexOf('-') && !ignore.test(is)) {
+      loaded.add(is);
+      options.on(is);
+    }
+  };
   /**
    * Start observing a document, or a specific container, and automatically
    * download once Custom Elements from a specific path
@@ -12,45 +25,33 @@ self.uceLoader = (function (exports) {
    * @returns {MutationObserver} the disconnect-able `container` observer
    */
 
+
   var index = (function (options) {
     var target = options.container || document;
-
-    var load = function load(mutations) {
-      for (var i = 0, length = mutations.length; i < length; i++) {
-        for (var addedNodes = mutations[i].addedNodes, j = 0, _length = addedNodes.length; j < _length; j++) {
-          var node = addedNodes[j];
-          var children = node.children,
-              getAttribute = node.getAttribute,
-              tagName = node.tagName;
-
-          if (getAttribute) {
-            var is = (getAttribute.call(node, 'is') || tagName).toLowerCase();
-
-            if (0 < is.indexOf('-') && !loaded.has(is) && !ignore.test(is)) {
-              loaded.add(is);
-              options.on(is);
-            }
-
-            crawl(children);
-          }
-        }
-      }
-    };
-
-    var crawl = function crawl(addedNodes) {
-      load([{
-        addedNodes: addedNodes
-      }]);
-    };
-
-    crawl(document == target ? target.documentElement.children : [target]);
-    var observer = new MutationObserver(load);
+    var bound = all.bind(options);
+    bound([{
+      addedNodes: document == target ? target.documentElement.children : [target]
+    }]);
+    var observer = new MutationObserver(bound);
     observer.observe(target, {
       subtree: true,
       childList: true
     });
     return observer;
   });
+
+  function all(mutations) {
+    for (var i = 0, length = mutations.length; i < length; i++) {
+      for (var addedNodes = mutations[i].addedNodes, j = 0, _length = addedNodes.length; j < _length; j++) {
+        var node = addedNodes[j];
+        if (node.getAttribute) add(this, node);
+
+        for (var result = expression.evaluate(node, ORDERED_NODE_SNAPSHOT_TYPE, null), snapshotLength = result.snapshotLength, k = 0; k < snapshotLength; k++) {
+          add(this, result.snapshotItem(k));
+        }
+      }
+    }
+  }
 
   exports.default = index;
 
